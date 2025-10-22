8d53786f1f6462967eb00e1a03156a4802098ca1
Kyle Robbertze <kyle@paddatrapper.com>
2025-10-23 00:18:28 +0100

    fix: correct type hint for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@
 import { createRouter, createWebHistory } from "vue-router/auto";
 import { setupLayouts } from "virtual:generated-layouts";
 import { routes } from "vue-router/auto-routes";
+import type { RouteLocationNormalizedGeneric } from "vue-router/auto";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +16,7 @@ const router = createRouter({
 });
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
+router.onError((err: Error | undefined, to: RouteLocationNormalizedGeneric) => {
   if (err?.message?.includes?.("Failed to fetch dynamically imported module")) {
     if (!localStorage.getItem("vuetify:dynamic-reload")) {
       console.log("Reloading page to fix dynamic import error");
